9edeb599ad5c38451ceccb12f20d38b08f7a027d
james <james2004.coward@gmail.com>
2023-10-05 20:08:00 +0100

    Added all files

---
+++ test.c
@@ -0,0 +1,26 @@
+asm ("ldr r13, =$0xFFFFF");
+
+struct test {
+    int t1;
+    int t2;
+} test;
+
+int main(int argc, char **argv) {
+    
+
+    int a = 12; int b = 14;
+
+    for (int i = 0; i < 10; i++) {
+        a = a + b;
+        b = b << 1;
+    }
+    
+    struct test astruct = {1,3};
+
+    int c = astruct.t1 + astruct.t2;
+
+    char* stra = "Hello\0";
+    char* strb = "World\0";
+
+    return 0;
+}

35961ac4cff684c1718e31a974fce0b6278ba3d2
James C <psyjc25@nottingham.ac.uk>
2023-10-05 21:18:32 +0100

    changed test3

--- test3.c
+++ test3.c
@@ -1,9 +1,18 @@
 asm ("ldr r13, =$0x10000");
 
 int main(int argc,  char **argv) {
-	int a = 1;
-	int b = 2;
+	int arr[] = {1,2,3,4,5};
 
-	int c = a + b;
-	return c;
+	int tot = 0;
+	int len = sizeof(arr) / sizeof(int);
+	for (int i = 0; i < len; i++) {
+		tot += arr[i] << 1;
+		tot -= i % 2 == 0 ? 1 : 0;
+	}
+
+	for (int i = 0; i < len; i++) {
+		arr[i] = tot * arr[i] + 1 << i;
+	}
+
+	return arr[0];
 }
